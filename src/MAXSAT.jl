4bf6a6ab1d0771f375f33419c93ead6ff4a85bca
Guenther Raidl <raidl@ac.tuwien.ac.at>
2019-11-21 10:44:01 +0100

    Further work on MAXSAT

---
+++ src/MAXSAT.jl
@@ -0,0 +1,248 @@
+export MAXSATInstance, MAXSATSolution, copy!, copy
+
+"""MAXSATInstance
+
+A MAXSAT problem instance.
+
+The goal is to maximize the number of clauses satisfied in a boolean function given in conjunctive normal form.
+
+Attributes
+    - n: number of variables, i.e., size of incidence vector
+    - m: number of clauses
+    - clauses: vector of clauses, where each clause is represented by a
+        vector of integers;
+        a positive integer v refers to the v-th variable, while a negative
+        integer v refers to the negated form of the v-th variable
+    - variable_usage: vector containing for each variable a vector with the
+        indices of the clauses in which the variable appears;
+        needed for efficient incremental evaluation
+"""
+struct MAXSATInstance
+    n::Int
+    m::Int
+    clauses::Vector{Vector{Int}}
+    variable_usage::Vector{Vector{Int}}
+end
+
+""" MAXSATInstance
+
+Read a MAXSATInstance from the given file.
+"""
+function MAXSATInstance(file_name::String)
+    local n::Int
+    local m::Int
+    clauses = Vector{Vector{Int}}()
+    local variable_usage::Vector{Vector{Int}}
+    open(file_name) do f
+        for line in eachline(f)
+            if line[1] == 'c'
+                # ignore comments
+                continue
+            end
+            fields = split(line)
+            if length(fields) == 4 && fields[1] == "p" && fields[2] == "cnf"
+                # parse header line with n and m
+                n = parse(Int, fields[3])
+                m = parse(Int, fields[4])
+                variable_usage = [Vector{Int}() for _ in 1:n]
+            elseif length(fields) >= 1
+                # parse clause
+                clause = [parse(Int, s) for s in fields[1:length(fields)-1]]
+                push!(clauses, clause)
+                for v in clause
+                    push!(variable_usage[abs(v)], length(clauses))
+                end
+            end
+        end
+    end
+    @assert n > 0 && m > 0
+    @assert length(clauses) == m
+    MAXSATInstance(n, m, clauses, variable_usage)
+end
+
+inst = MAXSATInstance("../data/maxsat-simple.cnf")
+
+
+"""MAXSATSolution
+
+A concrete solution type to solve the MAXSAT problem.
+"""
+mutable struct MAXSATSolution{N} <: BoolVectorSolution{N}
+    inst::MAXSATInstance
+    obj_val::Int
+    obj_val_valid::Bool
+    x::MVector{N,Bool}
+end
+
+"""MAXSATSolution(::MAXSATInstance)
+
+Create a solution object for the given `MAXSATInstance`.
+"""
+MAXSATSolution(inst::MAXSATInstance) =
+    MAXSATSolution{inst.n}(inst, -1, false, MVector{inst.n, Bool}(undef))
+
+function copy!(s1::S, s2::S) where {S <: MAXSATSolution}
+    s1.inst = s2.inst
+    s1.obj_val = s2.obj_val
+    s1.obj_val_valid = s2.obj_val_valid
+    s1.x[:] = s2.x
+end
+
+copy(s::MAXSATSolution) =
+    MAXSATSolution{s.inst.n}(s.inst, -1, false, Base.copy(s.x[:]))
+
+"""calc_objective(::MAXSATSolution)
+
+Count the number of satisfied clauses.
+"""
+function calc_objective(s::MAXSATSolution)
+    satisfied = 0
+    for clause in s.inst.clauses
+        for v in clause
+            if s.x[abs(v)] == (v > 0)
+                satisfied += 1
+                break
+            end
+        end
+    end
+    satisfied
+end
+
+#=
+
+function local_improve(self, par, _result)
+    """Perform one k_flip_neighborhood_search."""
+    x = self.x
+    obj_val = self.obj()
+    new_obj_val = k_flip_neighborhood_search!(x, obj_val, self.inst.julia_inst, par, false)
+    if new_obj_val > obj_val
+        PyArray(self."x")[:] = x
+        self.obj_val = new_obj_val
+        return true
+    end
+    return false
+end
+
+function shaking(self, par, _result)
+    """Scheduler method that performs shaking by flipping par random positions."""
+    x = PyArray(self."x")
+    for i in 1:par
+        p = rand(1:length(x))
+        x[p] = !x[p]
+    end
+    self.invalidate()
+end
+
+function destroy(self, par, _result)
+    """Destroy operator for ALNS selects par*ALNS.get_number_to_destroy positions
+    uniformly at random for removal.
+
+    Selected positions are stored with the solution in list self.destroyed.
+    """
+    x = PyArray(self."x")
+    num = min(ALNS.get_number_to_destroy(length(x)) * par, length(x))
+    self.destroyed = sample(1:length(x), num, replace=false)
+    self.invalidate()
+end
+
+function repair(self, _par, _result)
+    """Repair operator for ALNS assigns new random values to all positions in self.destroyed."""
+    @assert !(self.destroyed === nothing)
+    x = PyArray(self."x")
+    for p in self.destroyed
+        x[p] = rand(0:1)
+    end
+    self.destroyed = nothing
+    self.invalidate()
+end
+
+
+function crossover(self, other)
+    """ Perform uniform crossover as crossover."""
+    return self.uniform_crossover(other)
+end
+
+
+function k_flip_neighborhood_search!(x::Vector{Bool}, obj_val::Int, julia_inst::JMAXSATInstance,
+                                 k::Int, best_improvement::Bool)::Int
+"""Perform one major iteration of a k-flip local search, i.e., search one neighborhood.
+
+If best_improvement is set, the neighborhood is completely searched and a best neighbor is
+kept; otherwise the search terminates in a first-improvement manner, i.e., keeping a first
+encountered better solution.
+
+:returns: Objective value.
+"""
+len_x = length(x)
+@assert 0 < k <= len_x
+better_found = false
+best_sol = copy(x)
+best_obj = obj_val
+perm = randperm(len_x)  # random permutation for randomizing enumeration order
+p = fill(-1, k)  # flipped positions
+# initialize
+i = 1  # current index in p to consider
+while i >= 1
+    # evaluate solution
+    if i == k + 1
+        if obj_val > best_obj
+            if !best_improvement
+                return true
+            end
+            best_sol[:] = x
+            best_obj = obj_val
+            better_found = true
+        end
+        i -= 1  # backtrack
+    else
+        if p[i] == -1
+            # this index has not yet been placed
+            p[i] = (i>1 ? p[i-1] : 0) + 1
+            obj_val = flip_variable!(x, perm[p[i]], julia_inst, obj_val)
+            i += 1  # continue with next position (if any)
+        elseif p[i] < len_x - (k - i)
+            # further positions to explore with this index
+            obj_val = flip_variable!(x, perm[p[i]], julia_inst, obj_val)
+            p[i] += 1
+            obj_val = flip_variable!(x, perm[p[i]], julia_inst, obj_val)
+            i += 1
+        else
+            # we are at the last position with the i-th index, backtrack
+            obj_val = flip_variable!(x, perm[p[i]], julia_inst, obj_val)
+            p[i] = -1  # unset position
+            i -= 1
+        end
+    end
+end
+if better_found
+    x[:] = best_sol
+    obj_val = best_obj
+end
+return obj_val
+end
+
+
+function flip_variable!(x::Vector{Bool}, pos::Int, julia_inst::JMAXSATInstance, obj_val::Int)::Int
+val = !x[pos]
+x[pos] = val
+for clause in view(julia_inst.variable_usage,:,pos)
+    if clause == 0 break end
+    fulfilled_by_other = false
+    val_fulfills_now = false
+    for v in view(julia_inst.clauses,:,clause)
+        if v == 0 break end
+        if abs(v) == pos
+            val_fulfills_now = (v>0 ? val : !val)
+        elseif x[abs(v)] == (v>0 ? 1 : 0)
+            fulfilled_by_other = true
+            break  # clause fulfilled by other variable, no change
+        end
+    end
+    if !fulfilled_by_other
+        obj_val += (val_fulfills_now ? 1 : -1)
+    end
+end
+return obj_val
+end
+
+=#
--- src/MHLib.jl
+++ src/MHLib.jl
@@ -1,5 +1,185 @@
+
+"""MHLib
+
+A Toolbox for Metaheuristics and Hybrid Optimization Methods
+"""
 module MHLib
 
+using StaticArrays
+using Random
+
+export greet
+
 greet() = print("Hello World!")
+
+export Solution, to_maximize, obj!, invalidate!, is_better, is_worse,
+    is_better_obj, is_worse_obj, dist, check
+
+"""Solution
+
+An abstract solution to an optimization problem.
+
+Concrete subtypes need to implement:
+- `obj_val`: objective value of solution, usually some numerical type
+- `obj_val_valid::Bool`: indicates if obj_val is valid
+- `calc_objective(::Solution): calculate and return objective value of solution
+- `to_maximize(::Type)`: for minimization this method must return false
+- `copy!(::Solution, ::Solution)`: make first solution a copy of the second
+- `copy(::Solution)`: return an independent copy of the solution
+"""
+abstract type Solution end
+
+"""to_maximize(::Type)
+
+Return true if the optimization goal is to maximize the objective function
+in the given type of solutions.
+
+This default implementation returns true.
+"""
+to_maximize(::Type) = true
+to_maximize(s::Solution) = to_maximize(typeof(s))
+
+"""obj!(::Solution)
+
+Return obj_val if obj_val_valid or calculate it via objective(::Solution).
+"""
+obj!(s::Solution) = s.obj_val_valid ? s.obj_value : calc_objective(s)
+
+"""invalidate!(::Solution)
+
+Invalidate a possibly cached objective value, usually due to a change in the solution.
+"""
+invalidate!(solution::Solution) = obj_val_valid = false;
+
+"""is_better(::Solution, ::Solution)
+
+Return true if the first solution is better than the second.
+"""
+function is_better(s1::S, s2::S) where S <: Solution
+    to_maximize(s1) ? obj!(s1) > obj!(s2) : obj!(s1) < obj!(s2)
+end
+
+"""is_worse(::Solution, ::Solution)
+
+Return true if the first solution is worse than the second.
+"""
+function is_worse(s1::S, s2::S) where S <: Solution
+    to_maximize(s1) ? obj!(s1) < obj!(s2) : obj!(s1) > obj!(s2)
+end
+
+"""is_better_obj(::Solution, obj1, obj2)
+
+Return true if obj1 is a better objective value than obj2 in
+the given solution type.
+"""
+function is_better_obj(s::Solution, obj1, obj2)
+    to_maximize(s) ? obj1 > obj2 : obj1 < obj2
+end
+
+"""is_worse_obj(::Solution, obj1, obj2)
+
+Return true if obj1 is a worse objective value than obj2 in
+the given solution type.
+"""
+function is_worse_obj(s::Solution, obj1, obj2)
+    to_maximize(s) ? obj1 < obj2 : obj1 > obj2
+end
+
+"""distthe distance of s1 to s2
+
+Return true if the first solution is worse than the second.
+The default implementation just returns false if the solutions are the same
+and true otherwise.
+
+"""
+dist(s1::S, s2::S) where S <: Solution = s1 != s2
+
+"""Check validity of solution.
+
+If a problem is encountered, raise an exception.
+The default implementation just re-calculates the objective value.
+"""
+function check(s::Solution)::Nothing
+    if s.obj_val_valid
+        old_obj = self.obj_val
+        invalidate!(s)
+        if old_obj != s.obj!()
+            error("Solution has wrong objective value: $old_obj, " *
+                  "should be $(s.obj!())")
+        end
+    end
+end
+
+
+export VectorSolution, copy!, len
+
+"""VectorSolution
+
+An abstract solution encoded by a vector of length `N` and type `T`.
+
+Concrete subtypes need to implement:
+- all requirements of the supertype `Solution`
+- `x::AbstractVector`: vector representing the solution
+"""
+abstract type VectorSolution{N,T} <: Solution end
+
+"""copy!(::Solution, ::Solution)
+
+Make first solution an independent copy of the second solution.
+"""
+function copy!(s1::S, s2::S) where S <: VectorSolution
+      # copy!(s1, s2)
+end
+
+"""len(::VectorSolution)
+
+Length of the solution vector.
+"""
+len(s::VectorSolution) = length(s.x)
+
+
+export BoolVectorSolution
+
+"""BoolVectorSolution
+
+An abstract solution encoded by a fixed-length boolean vector.
+"""
+abstract type BoolVectorSolution{N} <: VectorSolution{N,Bool} end
+
+initialize!(s::BoolVectorSolution) = ( rand!(s.x); invalidate!(s) )
+
+
+"""OneMaxSolution
+
+A concrete solution type to solve the MAXSAT problem.
+"""
+mutable struct OneMaxSolution{N} <: BoolVectorSolution{N}
+    obj_val::Int
+    obj_val_valid::Bool
+    x::MVector{N,Bool}
+    OneMaxSolution{N}() where {N} = new{N}(-1, false, MVector{N,Bool}(undef))
+    OneMaxSolution{N}(s::OneMaxSolution{N}) where {N} =
+        new{N}(s.obj_val, s.obj_val_valid, MVector{N,Bool}(s.x))
+end
+
+calc_objective(s::OneMaxSolution) = sum(s.x)
+
+function copy!(s1::S, s2::S) where {S <: OneMaxSolution}
+    s1.obj_val = s2.obj_val
+    s1.obj_val_valid = s2.obj_val_valid
+    s1.x[:] = s2.x
+end
+
+copy(s::OneMaxSolution) = deepcopy(s)
+
+
+include("MAXSAT.jl")
+
+
+
+
+s = OneMaxSolution{5}()
+initialize!(s)
+print("$s, $(obj!(s))")
 
 end # module
