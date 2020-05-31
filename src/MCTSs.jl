5e36f6bf9fa16496e78e05d43697b6040b0e2899
Guenther Raidl <raidl@ac.tuwien.ac.at>
2020-05-31 15:44:32 +0200

    argmax in MCTS randomized in case of ties

--- src/MCTSs.jl
+++ src/MCTSs.jl
@@ -30,6 +30,20 @@ end
 
 
 """
+    argmax_rand(vector)
+
+Return index of maximum value in vector and in case of ties make a random selection.
+"""
+function argmax_rand(a::Vector)
+    indices = findall(a.==maximum(a))
+    if length(indices) > 1
+        rand(indices)
+    else
+        indices[1]
+    end
+end
+
+"""
     MCTS
 
 Monte Carlo Tree Search.
@@ -97,7 +111,7 @@ N!(node::Node, n) = (node.parent.child_N[node.action] = Int32(n))
 W(node::Node) = node.parent.child_W[node.action]
 W!(node::Node, n) = (node.parent.child_W[node.action] = Float32(n))
 
-child_Q(node::Node) = node.child_W ./ node.child_N
+child_Q(node::Node) = node.child_W ./ (1 .+ node.child_N)
 
 child_U(node::Node) = sqrt(N(node)) .* node.child_P ./ (1 .+ node.child_N)
 
@@ -105,7 +119,7 @@ function best_action(node::Node)::Int
     child_score = child_Q(node) + node.mcts.c_puct * child_U(node)
     masked_child_score = child_score
     masked_child_score[.~node.valid_actions] .= typemin(Float32)
-    return argmax(masked_child_score)
+    return argmax_rand(masked_child_score)
 end
 
 function select_leaf(node::Node)::Node
@@ -200,7 +214,7 @@ function compute_action!(mcts::MCTS, node::Node)
         end
         backup(leaf, value)
     end
-    return argmax(node.child_N)
+    return argmax_rand(node.child_N)
 end
 
 """
