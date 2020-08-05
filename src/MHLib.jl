75100cae33e55ff132fda99192fd067446524c38
Thomas Jatschka <tjatschk@ac.tuwien.ac.at>
2020-08-05 16:55:19 +0200

    first draft for subsetvec solution

--- src/MHLib.jl
+++ src/MHLib.jl
@@ -312,6 +312,263 @@ function flip_variable!(s::BoolVectorSolution, pos::Int)
 end
 
 
+#----------------------------- SubsetVectorSolution ------------------------------
+export SubsetVectorSolution, clear!
+
+"""
+    SubsetVectorSolution
+
+A generic class for solutions that are arbitrary cardinality subsets of a given set represented in vector form.
+
+"""
+abstract type SubsetVectorSolution{T} <: VectorSolution{T} end
+
+function clear!(s::SubsetVectorSolution)
+    s.sel = 0
+    s.invalidate()
+end
+
+"""Scans elements from pool (by default in random order) and selects those whose inclusion is feasible.
+
+Elements in pool must not yet be selected.
+Parameter pool must either be None, in which case x[sel:end] is used as pool,
+or x[sel:_] for some _ > sel.
+If random_order is set, the elements in the pool are processed in random order.
+Uses element_added_delta_eval()
+Reorders elements in pool so that the selected ones appear in pool[begin:return-value].
+"""
+function fill!(s::SubsetVectorSolution, pool::Vector{T}, random_order::Bool=true)
+    if !may_be_extendible(s)
+        return 0
+    end
+    x = s.x
+    if pool == nothing
+        pool = x[s.sel+1:end]
+    end
+
+    selected = 1
+    for i in 1:len(pool)
+        if random_order:
+            ir = rand(i:lenght(pool))
+            if selected != ir
+                pool[selected], pool[ir] = pool[ir], pool[selected]
+            end
+        end
+        s.sel += 1
+        x[s.sel] = pool[selected]
+        if element_added_delta_eval!(s)
+            selected += 1
+            if !may_be_extendible(s)
+                break
+            end
+        end
+    end
+    if selected > 0
+        sort_sel!(s)
+    end
+    return selected
+end
+
+"""Removes min(k,sel) randomly selected elements from the solution.
+
+Uses element_removed_delta_eval, which should be overloaded and adapted to the problem.
+The elements are removed even when the solution becomes infeasible.
+"""
+function remove_some!(s::SubsetVectorSolution, k::Int)
+    x = s.x
+    k = min(k, s.sel)
+    if k > 0:
+        for i in 1:k
+            j = rand(1:s.sel)
+            s.sel -= 1
+            if j != s.sel
+                x[j], x[s.sel] = x[s.sel], x[j]
+            end
+            element_removed_delta_eval!(s,allow_infeasible=true)
+        sort_sel!(s)
+
+
+"""Random construction of a new solution by applying fill to an initially empty solution."""
+function initialize!(s::SubsetVectorSolution, k::Int)
+    clear!(s)
+    fill!(s,s.all_elements)
+    invalidate!(s)
+end
+
+"""Check correctness of solution; throw an exception if error detected.
+
+:param unsorted: if set, it is not checked if s is sorted
+"""
+function check(s::SubsetVectorSolution, unsorted::Bool=false)
+    length = length(s.all_elements)
+    if !(1 <= s.sel <= length)
+        error("Invalid attribute sel in solution: $(s.sel)")
+    end
+    if length(s.x) != length
+        error("Invalid length of solution array x: $(s.x)")
+    end
+    if Set(s.x) != s.all_elements
+        error("Invalid solution - x is not a permutation of V: $(s.x) (sorted: $(sorted!(s.x)))")
+    else
+        sol_set = Set(s.x[begin:s.sel])
+        if !issubset(sol_set, s.all_elements) or lenght(sol_set) != s.sel:
+            error("Solution not simple subset of V: $(s.x[begin:s.sel]), $(s.all_elements)")
+        end
+    end
+    if !unsorted
+        old_v = s.x[1]
+        for v in s.x[2:s.sel]
+            if v <= old_v
+                error("Solution not sorted: value $(v) in $(s.x)")
+            end
+            old_v = v
+        end
+    end
+
+    if s.obj_val_valid
+        old_obj = s.obj_val
+        invalidate!(s)
+        if old_obj != obj(s)
+            error("Solution has wrong objective value: $(old_obj) should be $(obj(s))")
+        end
+    end
+
+"""Search 2-exchange neighborhood followed by fill!() with random ordering.
+
+Each selected location is tried to be exchanged with each unselected one followed by a fill().
+
+The neighborhood is searched in a randomized fashion.
+Overload the methods element_removed_delta_eval and element_added_delta_eval for an efficient problem-specific
+delta evaluation.
+Returns true if the solution could be improved, otherwise the solution remains unchanged.
+"""
+function two_exchange_random_fill_neighborhood_search!(s::SubsetVectorSolution, best_improvement::Bool)
+    sel = s.sel
+    x = s.x
+    orig_obj = obj(s)
+    self_backup = nothing
+    x_sel_orig = copy(x[begin:sel])
+    shuffle!(x[begin:sel])
+    best = copy(s)
+    num_neighbors = 0
+    for (i, v) in enumerate(x[begin:sel])
+        # move v at the end of the selection range
+        if i != sel
+            x[i], x[sel] = x[sel], x[i]
+        end
+        # delete v by reducing the selection range by one
+        s.sel -= 1
+        element_removed_delta_eval!(s,allow_infeasible=True)
+        obj1 = obj(s)
+        pool = get_extension_pool(s)
+        shuffle!(pool)
+
+        # search v (the deleted item) and place it at the front of the extension pool
+        v_pos = findall(x->x==v)[1]
+        if v_pos != 1
+            pool[1], pool[v_pos] = pool[v_pos], pool[1]
+        end
+
+        # enumerate over all items in the extension pool except for v
+        for (j, vu) in enumerate(pool[2:end])
+
+            # increase selection range by one
+            # and swap the element at the last position in the selction range
+            #with a new element from the pool
+            x[sel], pool[j+1] = vu, x[sel]
+            s.sel += 1
+
+            num_neighbors += 1
+            if element_added_delta_eval(s)
+                # neighbor is feasible
+                random_fill_applied = false
+                if may_be_extendible(s)
+                    self_backup = copy(s)
+                    fill!(get_extension_pool(s))
+                    random_fill_applied = true
+                end
+                if is_better(s,best)
+                    # new best solution found
+                    if not best_improvement
+                        sort_sel!(s)
+                        return true
+                    end
+                    copy!(s, best)
+                end
+                if random_fill_applied
+                    if i != s.sel
+                        x[i], x[sel] = x[sel], x[i]
+                    end
+                    copy!(s, self_backup)
+                end
+                s.sel -= 1
+                element_removed_delta_eval(s, update_obj_val=false, allow_infeasible=true)
+                s.obj_val = obj1
+            end
+            x[sel], pool[j+1] = pool[j+1], vu
+        end
+        s.sel += 1
+        element_added_delta_eval(s,update_obj_val=false, allow_infeasible=true)
+        s.obj_val = orig_obj
+        if i != sel-1
+            x[i], x[sel] = x[sel], x[i]
+        end
+    end
+    if is_better_obj(s, best.obj(), orig_obj)
+        # return new best solution
+        copy!(s, best)
+        sort_sel!(s)
+        return true
+    end
+    x[begin:sel] = x_sel_orig
+    return false
+end
+
+"""Return a list of yet unselected elements that may possibly be added."""
+function get_extension_pool(s::SubsetVectorSolution)
+    return s.x[s.sel+1:end]
+
+"""Quick check if the solution has chances to be extended by adding further elements."""
+function may_be_extendible(s::SubsetVectorSolution):
+    return s.sel < length(s.x)
+
+"""Element x[sel] has been removed in the solution, if feasible update other solution data,
+else revert.
+
+This is a helper function for delta-evaluating solutions when searching a neighborhood that needs
+    to be overloaded for a concrete problem.
+        It can be assumed that the solution was in a correct state with a valid objective value in obj_val
+        *before* the already applied move, obj_val_valid therefore is True.
+        The default implementation just calls invalidate() and returns True.
+
+        :param update_obj_val: if set, the objective value should also be updated or invalidate needs to be called
+        :param allow_infeasible: if set and the solution is infeasible, the move is nevertheless accepted and
+        the update of other data done
+        :return: True if feasible, False if infeasible
+        """
+function element_removed_delta_eval(s::SubsetVectorSolution, update_obj_val=rrue, allow_infeasible=false)
+    if update_obj_val
+        invalidate(s)
+    return true
+
+"""Element x[sel-1] was added to a solution, if feasible update further solution data, else revert.
+
+This is a helper function for delta-evaluating solutions when searching a neighborhood that needs
+    to be overloaded for a concrete problem.
+        It can be assumed that the solution was in a correct state with a valid objective value in obj_val
+        *before* the already applied move, obj_val_valid therefore is True.
+        The default implementation just calls invalidate() and returns True.
+
+        :param update_obj_val: if set, the objective value should also be updated or invalidate needs to be called
+        :param allow_infeasible: if set and the solution is infeasible, the move is nevertheless accepted and
+        the update of other data done
+        :return: True if feasible, False if infeasible
+        """
+function element_added_delta_eval(s::SubsetVectorSolution, update_obj_val=true, allow_infeasible=false):
+    if update_obj_val
+        invalidate(s)
+    return true
+
 #-----------------------------------------------------------
 
 include("settings.jl")
